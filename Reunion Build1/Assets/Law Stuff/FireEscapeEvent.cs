669400c7bf64fbda8b55b064a91486fa8ce044c6
laurence <l.peat@saedubai.com>
2018-04-24 16:47:36 +0400

    door & fire event

---
+++ Reunion Build1/Assets/Law Stuff/FireEscapeEvent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FireEscapeEvent : MonoBehaviour {
+
+    public GameObject player;
+    public AudioSource AKTUMALARMEH;
+    public Text runTEXT, exitTEXT, overHereTEXT;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+       
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+
+            AKTUMALARMEH.Play();
+            Debug.Log("collided");
+            GameObject.FindGameObjectWithTag("enterDoor").GetComponent<Doorcontrol>().ChangeDoorState();
+            GameObject.FindGameObjectWithTag("exitDoor").GetComponent<Doorcontrol>().ChangeDoorState();
+            exitTEXT.gameObject.SetActive(false);
+            runTEXT.gameObject.SetActive(true);
+            overHereTEXT.gameObject.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+
+}
